3529821bd78207a2edf591f5973fba8276daba1f
Manpreet singh <manijagdev2401@gmail.com>
2023-05-25 20:38:15 +0530

    Workiong on pdf component

--- src/components/pdf-js-express/index.tsx
+++ src/components/pdf-js-express/index.tsx
@@ -8,20 +8,68 @@ const PdfJsExpress = () => {
   useEffect(() => {
     WebViewer(
       {
-        path: "/webviewer/lib", 
-        initialDoc: "/files/dummy.pdf",
-        // licenseKey: 'VMeLR5MsW5lX3X9YfqQF',
+        path: "node_modules/@pdftron/pdfjs-express/public",
+        initialDoc: "/src/assets/pdf/sample.pdf",
       },
       viewer.current
     ).then((instance: any) => {
-      console.log(" Result ", instance);
+      instance.UI.setHeaderItems(function (header: any) {
+        header.getHeader("toolbarGroup-Shapes").delete();
+
+        // hide the ribbons and second header
+        instance.UI.disableElements([
+          "ribbons",
+          "toolsHeader",
+          "annotationCommentButton",
+          "toggleNotesButton",
+          "menuButton",
+          "contextMenuPopup",
+          "annotationStylePopup",
+          "menuOverlay",
+        ]);
+        // instance.UI.disableElements(["toolsHeader"]);
+        // instance.UI.disableElements(["toolsHeader"]);
+        instance.UI.disableElement([instance.UI.Feature.MultiTab]);
+
+        const { documentViewer, annotationManager } = instance.Core;
+
+        documentViewer.setWatermark({
+          // Draw diagonal watermark in middle of the document
+          diagonal: {
+            fontSize: 25, // or even smaller size
+            fontFamily: "sans-serif",
+            color: "red",
+            opacity: 50, // from 0 to 100
+            text: "Just for testing",
+          },
+
+          // Draw header watermark
+          header: {
+            fontSize: 10,
+            fontFamily: "sans-serif",
+            color: "red",
+            opacity: 70,
+            left: "left watermark",
+            center: "center watermark",
+            right: "",
+          },
+        });
+
+        //Delete comment Icon
+        documentViewer.addEventListener("annotationsLoaded", () => {
+          const annots = annotationManager.getAnnotationsList();
+          // remove annotations
+          annotationManager.deleteAnnotations(annots);
+        });
+      });
     });
-  }, []); 
+  }, []);
 
   return (
-    <div className="MyComponent"> 
-      <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
+    <div>
+      <div className="MyComponent">
+        <div className="webviewer" ref={viewer} style={{}}></div>
+      </div>
     </div>
   );
 };
